9f2091618898b54b878fa1fd1eb7f00cd02a2638
SumitSingh-03 <sumittanwar4169@gmail.com>
2025-04-18 20:58:33 +0530

    second commit

--- application.py
+++ application.py
@@ -38,4 +38,7 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host='127.0.0.1',debug=True,port=5004)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
